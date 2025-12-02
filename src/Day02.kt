452f153237e94425a89e52ff5d292fe65c1430f2
Miguel Del Corso <migueldelcorso@gmail.com>
2025-12-02 11:45:09 -0300

    Made some improvements

--- src/Day02.kt
+++ src/Day02.kt
@@ -10,28 +10,20 @@ fun String.isInvalid(): Boolean {
     return doubled.drop(1).dropLast(1).contains(this)
 }
 
-private fun part1(input: List<LongRange>): Long {
-    var count = 0L
+private fun part1(input: List<LongRange>, isPart1: Boolean = true): Long {
+    var firstPart = 0L
+    var secondPart = 0L
     for(range in input) {
         for(i in range) {
             if(i.toString().isRepeatedTwice()) {
-                count += i
+                firstPart += i
             }
-        }
-    }
-    return count
-}
-
-private fun part2(input: List<LongRange>): Long {
-    var count = 0L
-    for(range in input) {
-        for(i in range) {
             if(i.toString().isInvalid()) {
-                count += i
+                secondPart += i
             }
         }
     }
-    return count
+    return if(isPart1) firstPart else secondPart
 }
 
 private val parseInput = { input: List<String> ->
@@ -44,10 +36,10 @@ private val parseInput = { input: List<String> ->
 fun main() {
     val testInput = parseInput(readInput("Day02_test"))
     check(part1(testInput) == 1227775554L)
-    check(part2(testInput) == 4174379265)
+    check(part1(testInput, false) == 4174379265)
 
     val input = parseInput(readInput("Day02"))
     check(part1(input) == 40214376723)
-    check(part2(input) == 50793864718)
+    check(part1(input, false) == 50793864718)
 }
  
