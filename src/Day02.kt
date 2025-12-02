57e21c2e58f0b9a7878a5da8f2acf0edcea148da
Miguel Del Corso <migueldelcorso@gmail.com>
2025-12-02 11:47:24 -0300

    Yet more improves and make em more kotlish

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,6 +1,5 @@
 fun String.isRepeatedTwice(): Boolean {
     if (this.length % 2 != 0) return false
-
     val half = this.length / 2
     return this.take(half) == this.substring(half)
 }
@@ -10,21 +9,14 @@ fun String.isInvalid(): Boolean {
     return doubled.drop(1).dropLast(1).contains(this)
 }
 
-private fun part1(input: List<LongRange>, isPart1: Boolean = true): Long {
-    var firstPart = 0L
-    var secondPart = 0L
-    for(range in input) {
-        for(i in range) {
-            if(i.toString().isRepeatedTwice()) {
-                firstPart += i
-            }
-            if(i.toString().isInvalid()) {
-                secondPart += i
-            }
-        }
-    }
-    return if(isPart1) firstPart else secondPart
+private val part = { input: List<LongRange>, isPart1: Boolean ->
+    input
+        .asSequence()
+        .flatMap { it.asSequence() }
+        .filter { if (isPart1) it.toString().isRepeatedTwice() else it.toString().isInvalid() }
+        .sum()
 }
+
 
 private val parseInput = { input: List<String> ->
     input.first().split(",").map { range ->
@@ -35,11 +27,11 @@ private val parseInput = { input: List<String> ->
 
 fun main() {
     val testInput = parseInput(readInput("Day02_test"))
-    check(part1(testInput) == 1227775554L)
-    check(part1(testInput, false) == 4174379265)
+    check(part(testInput, true) == 1227775554L)
+    check(part(testInput, false) == 4174379265)
 
     val input = parseInput(readInput("Day02"))
-    check(part1(input) == 40214376723)
-    check(part1(input, false) == 50793864718)
+    check(part(input, true) == 40214376723)
+    check(part(input, false) == 50793864718)
 }
  
