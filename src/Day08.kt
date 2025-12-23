53d331df38412121615fb1f0af0741d89089d72b
Miguel Del Corso <migueldelcorso@gmail.com>
2025-12-23 15:37:34 -0300

    not finished yet, doing some testing

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,43 +1,62 @@
 import kotlin.math.pow
 
-data class JunctionBox(val x: Double, val y: Double, val z: Double) {
+data class JunctionBox(val x: Double, val y: Double, val z: Double, var circuit: Int) {
     override fun toString(): String {
-        return "$x,$y,$z"
+        return "$x,$y,$z $circuit"
     }
+
     fun calculateDistanceTo(other: JunctionBox): Double {
         val (x2, y2, z2) = other
 
         return ((x2 - x).pow(2) + (y2 - y).pow(2) +
-                    (z2 - z).pow(2)).pow(0.5)
+                (z2 - z).pow(2)).pow(0.5)
     }
 }
+
 private fun parseInput(input: List<String>): List<JunctionBox> {
     return input.map {
-        val (x,y,z) = it.split(",").map { it.toInt() }
-        JunctionBox(x.toDouble(), y.toDouble(), z.toDouble())
+        val (x, y, z) = it.split(",").map { it.toInt() }
+        JunctionBox(x.toDouble(), y.toDouble(), z.toDouble(), 0)
     }
 }
 
 private fun part1(boxes: List<JunctionBox>): Int {
 
     var min = Double.MAX_VALUE
-    var minDistance: Pair<JunctionBox, JunctionBox> = Pair(JunctionBox(1.0, 0.0, 0.0), JunctionBox(1.0, 0.0, 0.0))
+    var minDistance: Pair<JunctionBox, JunctionBox> = Pair(JunctionBox(1.0, 0.0, 0.0, 0), JunctionBox(1.0, 0.0, 0.0, 0))
     var circuits = boxes.toMutableList()
 
     for (i in circuits.indices) {
         for (j in circuits.indices) {
-            if(i == j) continue
+            if (i == j) continue
+            if (circuits[j].circuit == circuits[i].circuit && circuits[i].circuit != 0) {
+                println("${circuits[i]} ${circuits[j]} are on the same circuit...")
+                continue
+            }
             val d = circuits[i].calculateDistanceTo(circuits[j])
 
-            if(min > d) {
+            if (min > d) {
                 min = d
                 minDistance = Pair(circuits[i], circuits[j])
             }
-//            println("comparing ${boxes[i]} to ${boxes[j]} d $d")
+//                println("comparing ${boxes[i]} to ${boxes[j]} d $d")
         }
     }
-    println(min)
+
     println(minDistance)
+    val latestCircuit = circuits.maxOf { it.circuit }
+
+    for (i in 0..<circuits.size) {
+        if (circuits[i] == minDistance.first) {
+            circuits[i].circuit = latestCircuit + 1
+        }
+        if (circuits[i] == minDistance.second) {
+            circuits[i].circuit = latestCircuit + 1
+        }
+    }
+    println(circuits)
+
+
     return 0
 }
 
@@ -49,7 +68,7 @@ fun main() {
     val testInput = parseInput(readInput("Day08_test"))
     check(part1(testInput) == 0)
 //    check(part2(testInput) == 0)
-     
+
 //    val input = readInput("Day08")
 //    check(part1(input) == 0)
 //    check(part2(input) == 0)
