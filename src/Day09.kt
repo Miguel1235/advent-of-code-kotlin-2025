6da03776ae6462083dbc02d43656c2f5b8cd476e
Miguel Del Corso <migueldelcorso@gmail.com>
2026-01-01 17:56:26 -0300

    Finished first part

---
+++ src/Day09.kt
@@ -0,0 +1,35 @@
+import kotlin.math.abs
+
+private fun part1(input: List<Tile>): Long {
+    val rectangles = input.combinations(2)
+
+    return rectangles.maxOf {
+        val (r1,c1) = it.first()
+        val (r2,c2) = it.last()
+        val base = abs(r1 - r2 )+ 1
+        val height = abs(c1 - c2)+ 1
+        base*height
+    }
+}
+
+private fun part2(input: List<String>): Int {
+    return 0
+}
+
+data class Tile(var r: Long, var c: Long)
+
+private val parseInput = { input: List<String> ->
+    input.map {
+        val (r,c) = it.split(",")
+        Tile(r.toLong(), c.toLong())
+    }
+}
+
+fun main() {
+    val testInput = parseInput(readInput("Day09_test"))
+    check(part1(testInput) == 50L)
+
+    val input = parseInput(readInput("Day09"))
+    check(part1(input) == 4761736832L)
+}
+ 
