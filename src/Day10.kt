76d7e63ab960c96bb30b7d46932c4bdc2b887a84
Miguel Del Corso <migueldelcorso@gmail.com>
2026-01-03 22:28:20 -0300

    Created next day, not yet started

---
+++ src/Day10.kt
@@ -0,0 +1,18 @@
+private fun part1(input: List<String>): Int {
+    return 0
+}
+
+private fun part2(input: List<String>): Int {
+    return 0
+}
+
+fun main() {
+    val testInput = readInput("Day10_test")
+    check(part1(testInput) == 0)
+    check(part2(testInput) == 0)
+     
+    val input = readInput("Day10")
+    check(part1(input) == 0)
+    check(part2(input) == 0)
+}
+ 
