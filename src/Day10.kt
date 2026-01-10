d2fd8806a17f1759721fdc2aa99fbbfe4ff2b711
Miguel Del Corso <migueldelcorso@gmail.com>
2026-01-10 18:10:37 -0300

    Parsed the input and created the class for the input on the day10th

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,17 +1,47 @@
-private fun part1(input: List<String>): Int {
+data class Machine(val goal: List<Char>, val buttons: List<List<Int>>, val joltage: List<Int>) {}
+
+private fun parseInput(input: List<String>): List<Machine> {
+    val machineRegex = Regex("""^\[([.#]+)]((?:\s\([0-9,]+\))+)\s\{([0-9,]+)}$""")
+    return input.map {
+        val (goal, buttons, joltage) = machineRegex.find(it)!!.groupValues.drop(1)
+
+        val goalList = goal.toList()
+        val buttonsList = buttons
+            .split(Regex("\\s+"))
+            .mapNotNull {
+                it.removeSurrounding("(", ")")
+                    .filter(Char::isDigit)
+                    .takeIf { it.isNotEmpty() }
+                    ?.toList()
+                    ?.map { it.digitToInt() }
+            }
+
+        val joltageList = joltage.toList()
+            .filter(Char::isDigit)
+            .map { it.digitToInt() }
+
+        Machine(goalList, buttonsList, joltageList)
+    }
+}
+
+private fun part1(machines: List<Machine>): Int {
+    for(machine in machines) {
+        println(machine)
+    }
     return 0
 }
 
-private fun part2(input: List<String>): Int {
+private fun part2(input: List<Machine>): Int {
     return 0
 }
 
 fun main() {
-    val testInput = readInput("Day10_test")
-    check(part1(testInput) == 0)
+    val testInput = parseInput(readInput("Day10_test"))
+
+    part1(testInput)
     check(part2(testInput) == 0)
      
-    val input = readInput("Day10")
+    val input = parseInput(readInput("Day10"))
     check(part1(input) == 0)
     check(part2(input) == 0)
 }
